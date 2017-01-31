3ac07e8499fefc9bcb7cc1260ecd90c09d7cafd5
mateoKaradza <mateo.karadza@gmail.com>
2017-01-31 11:28:34 +0100

    added password revealing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
+function handlePasswordReveal(e) {
+  if (e.checked)
+    return document.getElementById("password").type = "text";
+  document.getElementById("password").type = "password";
+}
+
 function emailError() {
   let div = document.createElement("div");
   let warningIcon = document.createElement("img");
   let span = document.createElement("span");
   const text = document.createTextNode("Please enter a valid email address.");
+
+  const email = document.getElementById("email");
 
   div.className = "alert";
   warningIcon.className = "warning";
